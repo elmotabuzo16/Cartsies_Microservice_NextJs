a8422fc8c4fd42e0859f4d21baf4c3e050ddf20e
elmotabuzo16 <elmotabuzojr.ust@gmail.com>
2024-10-09 23:02:22 +0800

    Calling Auction Service using Sync Message (HttpClient)

--- src/SearchService/Data/DbInitializer.cs
+++ src/SearchService/Data/DbInitializer.cs
@@ -3,6 +3,7 @@ using System.Text.Json;
 using MongoDB.Driver;
 using MongoDB.Entities;
 using SearchService.Entities;
+using SearchService.Services;
 
 namespace SearchService.Data;
 
@@ -20,19 +21,12 @@ public class DbInitializer
 
     var count = await DB.CountAsync<Item>();
 
-    if (count == 0)
+    using var scope = app.Services.CreateScope();
+    var httpClient = scope.ServiceProvider.GetRequiredService<AuctionSvcHttpClient>();
+    var items = await httpClient.GetItemsForSearchDb();
+
+    if (items.Count > 0)
     {
-      Console.WriteLine("No Data - will attempt to seed");
-
-      var itemData = await File.ReadAllTextAsync("Data/auctions.json");
-
-      var options = new JsonSerializerOptions
-      {
-        PropertyNameCaseInsensitive = true
-      };
-
-      var items = JsonSerializer.Deserialize<List<Item>>(itemData, options);
-
       await DB.SaveAsync(items);
     }
 
--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,7 +1,11 @@
+using System.Net;
 using MongoDB.Driver;
 using MongoDB.Entities;
+using Polly;
+using Polly.Extensions.Http;
 using SearchService.Data;
 using SearchService.Entities;
+using SearchService.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +13,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -20,13 +24,26 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+// Even if Auction Service is down, we can still get the results back
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitDb(app);
-}
-catch (Exception e)
-{
-    Console.WriteLine(e);
-}
+    try
+    {
+        await DbInitializer.InitDb(app);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+    }
+});
+
+
 
 app.Run();
+
+// If auction service is down
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError() // It will handle the exception
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3)); // Will keep trying until auction service is up
