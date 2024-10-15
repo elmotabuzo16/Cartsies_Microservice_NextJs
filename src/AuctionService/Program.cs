40ab8ab1ec720740dfa51f2f0d3f39c71064624e
elmotabuzo16 <elmotabuzojr.ust@gmail.com>
2024-10-15 20:26:46 +0800

    Adding docker for AuctionService

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -16,29 +16,35 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Mass Transit
 builder.Services.AddMassTransit(x =>
 {
-    // This is for Outbox (when the service is down)
-    x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
-    {
-        o.QueryDelay = TimeSpan.FromSeconds(10);
+  // This is for Outbox (when the service is down)
+  x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
+  {
+    o.QueryDelay = TimeSpan.FromSeconds(10);
 
-        o.UsePostgres();
-        o.UseBusOutbox();
+    o.UsePostgres();
+    o.UseBusOutbox();
+  });
+
+
+  x.UsingRabbitMq((context, cfg) =>
+  {
+    cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+    {
+      host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+      host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
     });
 
-
-    x.UsingRabbitMq((context, cfg) =>
-    {
-        cfg.ConfigureEndpoints(context);
-    });
+    cfg.ConfigureEndpoints(context);
+  });
 });
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
-        options.Authority = builder.Configuration["IdentityServiceUrl"];
-        options.RequireHttpsMetadata = false;
-        options.TokenValidationParameters.ValidateAudience = false;
-        options.TokenValidationParameters.NameClaimType = "username";
+      options.Authority = builder.Configuration["IdentityServiceUrl"];
+      options.RequireHttpsMetadata = false;
+      options.TokenValidationParameters.ValidateAudience = false;
+      options.TokenValidationParameters.NameClaimType = "username";
     });
 
 var app = builder.Build();
